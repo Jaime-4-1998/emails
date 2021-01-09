93c50388151a2ed7582ad42f7063cfed80c1551b
unknown <ramirezjaime648@gmailcom>
2021-01-08 18:16:13 -0600

    first commit

---
+++ mail_o2/js/lol.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+    });
+});
